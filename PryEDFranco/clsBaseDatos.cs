aab1a4c56dbda4c474a0e8957ad0914b59a14be2
root <mgiraudo@ies21.edu.ar>
2023-10-05 11:41:23 -0300

    .

---
+++ PryEDFranco/clsBaseDatos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using System.IO;
+namespace PryEDFranco
+{
+    internal class clsBaseDatos
+    {
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbCommand comando = new OleDbCommand();
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string CadenaConexion = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Libreria.mdb";
+        private string varCadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Libreria.mdb";
+
+
+
+        public void Listar(DataGridView Grilla)
+        {
+            try
+            {
+                conexion.ConnectionString = varCadenaConexion;
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = "Libro";
+
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, "libro");
+
+                Grilla.DataSource = null;
+                Grilla.DataSource = DS.Tables["libro"];
+
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+                conexion.Close();
+            }
+        }
+
+        public void Listar(DataGridView Grilla, String varInstruccionSQL)
+        {
+            try
+            {
+                conexion.ConnectionString = varCadenaConexion;
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = varInstruccionSQL;
+
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, "Resultado");
+
+                Grilla.DataSource = null;
+                Grilla.DataSource = DS.Tables["Resultado"];
+
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+                conexion.Close();
+            }
+        }
+
+
+    }
+
+
+
+
+}
